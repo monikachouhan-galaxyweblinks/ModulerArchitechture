1653e16bfb840ae6f0d9ecf1e94c6ef8714b4181
Monika_Chouhan <Salim@321>
2020-12-09 17:16:34 +0530

    Sync bug fix

--- features/authenticationLock/mpin/mpinauthentication/src/main/java/com/gwl/mpinlock/security/callbacks/PFPinCodeHelperCallback.java
+++
@@ -1,8 +0,0 @@
-package com.aucto.mpinlock.security.callbacks;
-
-
-import com.aucto.mpinlock.security.PFResult;
-
-public interface PFPinCodeHelperCallback<T> {
-    void onResult(PFResult<T> result);
-}
--- features/insta_feeds/src/main/java/com/gwl/feeds/presentation/AuthenticationListener.java
+++
@@ -1,5 +0,0 @@
-package com.aucto.feeds.presentation;
-
-public interface AuthenticationListener {
-    void onTokenReceived(String auth_token);
-}
