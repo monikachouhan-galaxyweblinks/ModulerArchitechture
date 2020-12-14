ec991ac6a6a87ce29e14b252799354f4fc29f096
Monika_Chouhan <Salim@321>
2020-12-14 21:15:40 +0530

    test

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@
  * All dependencies and gradle constants declared here.
  */
 object ApplicationId {
-    const val id = "com.gwl"
+     const val id = "com.gwl"
 }
 
 /**
@@ -25,7 +25,7 @@ object FeatureModules {
         Modules.signup
     )
 
-    val settingModules = arrayOf(
+     val settingModules = arrayOf(
         Modules.app,
         Modules.networking,
         Modules.exoplayerLib,
@@ -54,192 +54,192 @@ object FeatureModules {
  *  Every module used in project declare here.
  */
 object Modules {
-    val app = ":app"
-    val navigation = ":navigation"
-    val cache = ":common:cache"
+    const val app = ":app"
+    const val navigation = ":navigation"
+    const val cache = ":common:cache"
 
-    val core = ":common:core"
-    val networking = ":common:networking"
-    val presentation = ":common:core"
-    val model = ":common:model"
-    val home = ":features:home"
-    val settings = ":features:settings"
-    val login = ":features:login"
-    val exoplayerLib = ":libraries:exoplayer"
-    val playerCore = ":libraries:player-core"
-    val launcher = ":features:launcher"
-    val profile = ":features:profile"
+    const val core = ":common:core"
+    const val networking = ":common:networking"
+    const val presentation = ":common:core"
+    const val model = ":common:model"
+    const val home = ":features:home"
+    const val settings = ":features:settings"
+    const val login = ":features:login"
+    const val exoplayerLib = ":libraries:exoplayer"
+    const val playerCore = ":libraries:player-core"
+    const val launcher = ":features:launcher"
+    const val profile = ":features:profile"
 
-    /*val player = ":features:player_feeds"*/
-    val instaFeed = ":features:insta_feeds"
-    val signup = ":features:signup"
-    val mpinauthentication = ":features:authenticationLock:mpin:mpinauthentication"
-    val mpin = ":features:authenticationLock:mpin"
-    val fingure = ":features:authenticationLock:fingure"
-    val googleLogin = ":features:social_login:google_login"
-    val fbLogin = ":features:social_login:fblogin"
-    val search = ":features:search"
+    /*const val player = ":features:player_feeds"*/
+    const val instaFeed = ":features:insta_feeds"
+    const val signup = ":features:signup"
+    const val mpinauthentication = ":features:authenticationLock:mpin:mpinauthentication"
+    const val mpin = ":features:authenticationLock:mpin"
+    const val fingure = ":features:authenticationLock:fingure"
+    const val googleLogin = ":features:social_login:google_login"
+    const val fbLogin = ":features:social_login:fblogin"
+    const val search = ":features:search"
 }
 
 object Releases {
-    val versionCode = 1
-    val versionName = "1.0"
+    const val versionCode = 1
+    const val versionName = "1.0"
 }
 
 /**
  * Libraries version declare here.
  */
 object Versions {
-    val gradle = "3.5.2"
+    const val gradle = "3.5.2"
 
-    val compileSdk = 28
-    val minSdk = 18
-    val targetSdk = 28
+    const val compileSdk = 28
+    const val minSdk = 18
+    const val targetSdk = 28
 
-    val googleAuth = "16.0.1"
+    const val googleAuth = "16.0.1"
 
-    val googleServices = "4.3.3"
+    const val googleServices = "4.3.3"
 
-    val firebaseAuth = "16.0.4"
-    val firebaseCore = "16.0.4"
-    val facebookSdk = "4.30.0"
+    const val firebaseAuth = "16.0.4"
+    const val firebaseCore = "16.0.4"
+    const val facebookSdk = "4.30.0"
 
-    val fabric = "1.27.1"
-    val multidex = "2.0.1"
+    const val fabric = "1.27.1"
+    const val multidex = "2.0.1"
 
-    val appcompat = "1.0.2"
-    val design = "1.2.0-alpha03"
-    val cardview = "1.0.0"
-    val recyclerview = "1.0.0"
-    val constraint = "1.1.3"
-    val maps = "15.0.1"
+    const val appcompat = "1.0.2"
+    const val design = "1.2.0-alpha03"
+    const val cardview = "1.0.0"
+    const val recyclerview = "1.0.0"
+    const val constraint = "1.1.3"
+    const val maps = "15.0.1"
 
-    val ktx = "1.0.0-alpha1"
-    val coroutines = "1.2.0"
+    const val ktx = "1.0.0-alpha1"
+    const val coroutines = "1.2.0"
 
-    val kotlin = "1.3.50"
-    val timber = "4.7.1"
-    val rxjava = "2.2.6"
-    val rxkotlin = "2.3.0"
-    val rxAndroid = "2.1.1"
-    val retrofit = "2.5.0"
-    val loggingInterceptor = "3.12.1"
-    val glide = "4.9.0"
-    val rxpaper = "1.4.0"
-    val paperdb = "2.6"
-    val moshi = "1.8.0"
-    val lifecycle = "2.0.0"
-    val leakCanary = "1.6.3"
-    val crashlytics = "2.9.9"
-    val koin = "2.0.0-beta-1"
-    val shimmerVersion = "2.1.0"
-    val releaseVersion = "3.7.0.2905-A1"
-    val exoPlayer2Version = "2.9.6"
-    val butterKnifVersion = "10.1.0"
+    const val kotlin = "1.3.50"
+    const val timber = "4.7.1"
+    const val rxjava = "2.2.6"
+    const val rxkotlin = "2.3.0"
+    const val rxAndroid = "2.1.1"
+    const val retrofit = "2.5.0"
+    const val loggingInterceptor = "3.12.1"
+    const val glide = "4.9.0"
+    const val rxpaper = "1.4.0"
+    const val paperdb = "2.6"
+    const val moshi = "1.8.0"
+    const val lifecycle = "2.0.0"
+    const val leakCanary = "1.6.3"
+    const val crashlytics = "2.9.9"
+    const val koin = "2.0.0-beta-1"
+    const val shimmerVersion = "2.1.0"
+    const val releaseVersion = "3.7.0.2905-A1"
+    const val exoPlayer2Version = "2.9.6"
+    const val butterKnifVersion = "10.1.0"
 
-    val roomVersion = "2.2.3"
-    val roomCompiler = "2.2.3"
-    val playCore = "1.6.4"
-    val pagingVersion = "2.1.1"
+    const val roomVersion = "2.2.3"
+    const val roomCompiler = "2.2.3"
+    const val playCore = "1.6.4"
+    const val pagingVersion = "2.1.1"
 
-    val databindingCompiler = "3.5.3"
+    const val databindingCompiler = "3.5.3"
 
-    val junit = "4.12"
-    val assertjCore = "3.12.0"
-    val mockitoKotlin = "2.1.0"
-    val mockitoInline = "2.24.5"
+    const val junit = "4.12"
+    const val assertjCore = "3.12.0"
+    const val mockitoKotlin = "2.1.0"
+    const val mockitoInline = "2.24.5"
 }
 
 // Libraries used in modules
 object Libraries {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
 
-    val ktx = "androidx.core:core-ktx:${Versions.ktx}"
-    val multidex = "androidx.multidex:multidex:${Versions.multidex}"
+    const val ktx = "androidx.core:core-ktx:${Versions.ktx}"
+    const val multidex = "androidx.multidex:multidex:${Versions.multidex}"
 
-    val coroutinescore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
-    val coroutinesandroid =
+    const val coroutinescore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+    const val coroutinesandroid =
         "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
 
-    val kotlincoroutinesretrofit = "ru.gildor.coroutines:kotlin-coroutines-retrofit:1.1.0"
-    val loggingInterceptor =
+    const val kotlincoroutinesretrofit = "ru.gildor.coroutines:kotlin-coroutines-retrofit:1.1.0"
+    const val loggingInterceptor =
         "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}"
-    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-    val retrofitgson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
-    val exoPlayer = "com.google.android.exoplayer:exoplayer:${Versions.exoPlayer2Version}"
-    val butterknif = "com.jakewharton:butterknife:${Versions.butterKnifVersion}"
-    val butterknifCompiler = "com.jakewharton:butterknife-compiler:${Versions.butterKnifVersion}"
-    val flexBox = "com.google.android:flexbox:1.1.0"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val retrofitgson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    const val exoPlayer = "com.google.android.exoplayer:exoplayer:${Versions.exoPlayer2Version}"
+    const val butterknif = "com.jakewharton:butterknife:${Versions.butterKnifVersion}"
+    const val butterknifCompiler = "com.jakewharton:butterknife-compiler:${Versions.butterKnifVersion}"
+    const val flexBox = "com.google.android:flexbox:1.1.0"
 
     //paging
-    val paging = "androidx.paging:paging-runtime:${Versions.pagingVersion}"
+    const val paging = "androidx.paging:paging-runtime:${Versions.pagingVersion}"
 
-    val maps = "com.google.android.gms:play-services-maps:${Versions.maps}"
+    const val maps = "com.google.android.gms:play-services-maps:${Versions.maps}"
 
-    val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
 
-    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
-    val rxkotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
-    val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
 
-    val rxjavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
-    val moshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+    const val rxjavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    const val moshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
 
 
-    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
-    val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
 
     // Room
-    val roomDB = "androidx.room:room-ktx:${Versions.roomVersion}"
-    val roomCompiler = "androidx.room:room-compiler:${Versions.roomVersion}"
+    const val roomDB = "androidx.room:room-ktx:${Versions.roomVersion}"
+    const val roomCompiler = "androidx.room:room-compiler:${Versions.roomVersion}"
 
-    val paperdb = "io.paperdb:paperdb:${Versions.paperdb}"
-    val rxpaper = "com.github.pakoito:RxPaper2:${Versions.rxpaper}"
-    val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
+    const val paperdb = "io.paperdb:paperdb:${Versions.paperdb}"
+    const val rxpaper = "com.github.pakoito:RxPaper2:${Versions.rxpaper}"
+    const val moshi = "com.squareup.moshi:moshi:${Versions.moshi}"
 
-    val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
-    val lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    const val lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
 
-    val leakCanaryAndroid = "com.squareup.leakcanary:leakcanary-android:${Versions.leakCanary}"
-    val leakCanaryAndroidNoOp =
+    const val leakCanaryAndroid = "com.squareup.leakcanary:leakcanary-android:${Versions.leakCanary}"
+    const val leakCanaryAndroidNoOp =
         "com.squareup.leakcanary:leakcanary-android-no-op:${Versions.leakCanary}"
-    val leakCanaryAndroidSupportFragment =
+    const val leakCanaryAndroidSupportFragment =
         "com.squareup.leakcanary:leakcanary-support-fragment:${Versions.leakCanary}"
 
-    val crashlytics = "com.crashlytics.sdk.android:crashlytics:${Versions.crashlytics}"
-    val databinding = "androidx.databinding:databinding-compiler:${Versions.databindingCompiler}"
-    val koinAndroid = "org.koin:koin-android:${Versions.koin}"
-    val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
-    val shimmer = "io.supercharge:shimmerlayout:${Versions.shimmerVersion}"
+    const val crashlytics = "com.crashlytics.sdk.android:crashlytics:${Versions.crashlytics}"
+    const val databinding = "androidx.databinding:databinding-compiler:${Versions.databindingCompiler}"
+    const val koinAndroid = "org.koin:koin-android:${Versions.koin}"
+    const val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    const val shimmer = "io.supercharge:shimmerlayout:${Versions.shimmerVersion}"
 
 }
 
 object SupportLibraries {
-    val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
-    val design = "com.google.android.material:material:${Versions.design}"
-    val cardview = "androidx.cardview:cardview:${Versions.cardview}"
-    val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
-    val constrainLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraint}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val design = "com.google.android.material:material:${Versions.design}"
+    const val cardview = "androidx.cardview:cardview:${Versions.cardview}"
+    const val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
+    const val constrainLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraint}"
 }
 
 object GoogleLibraries {
-    val auth = "com.google.android.gms:play-services-auth:${Versions.googleAuth}"
-    val playCore = "com.google.android.play:core:${Versions.playCore}"
+    const val auth = "com.google.android.gms:play-services-auth:${Versions.googleAuth}"
+    const val playCore = "com.google.android.play:core:${Versions.playCore}"
 }
 
 object FirebaseLibraries {
-    val auth = "com.google.firebase:firebase-auth:${Versions.firebaseAuth}"
-    val core = "com.google.firebase:firebase-core:${Versions.firebaseCore}"
+    const val auth = "com.google.firebase:firebase-auth:${Versions.firebaseAuth}"
+    const val core = "com.google.firebase:firebase-core:${Versions.firebaseCore}"
 }
 
 object FacebookLibraries {
-    val sdk = "com.facebook.android:facebook-android-sdk:${Versions.facebookSdk}"
+    const val sdk = "com.facebook.android:facebook-android-sdk:${Versions.facebookSdk}"
 }
 
 object TestLibraries {
-    val junit = "junit:junit:${Versions.junit}"
-    val assertjCore = "org.assertj:assertj-core:${Versions.assertjCore}"
-    val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
-    val mockitoInline = "org.mockito:mockito-inline:${Versions.mockitoInline}"
-    val lifecycleTesting = "androidx.arch.core:core-testing:${Versions.lifecycle}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val assertjCore = "org.assertj:assertj-core:${Versions.assertjCore}"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val mockitoInline = "org.mockito:mockito-inline:${Versions.mockitoInline}"
+    const val lifecycleTesting = "androidx.arch.core:core-testing:${Versions.lifecycle}"
 }
