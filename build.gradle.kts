653630fcedf83ffc863541dd0e5cad6400d4dfd5
Monika_Chouhan <Salim@321>
2020-12-22 17:44:42 +0530

    update gradle and convert into kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.61' /*'1.3.61'*/
+    //ext.kotlin_version = '1.3.61' /*'1.3.61'*/
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:${Versions.gradle}"
-        classpath 'com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.6'
-        classpath "com.google.gms:google-services:${Versions.googleServices}"
+            classpath("com.android.tools.build:gradle:4.1.0")
+            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+            classpath("com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.6")
+            classpath("com.google.gms:google-services:${Versions.googleServices}")
 /*
         classpath "com.google.gms:google-services:${Versions.googleServices}"
         classpath "io.fabric.tools:gradle:${Versions.fabric}"
 */
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     }
 }
 
@@ -27,7 +27,6 @@ allprojects {
 
     }
 }
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+//include ':app',':common:networking',':libraries:exoplayer',":libraries:player-core",":features:profile",":features:player_feeds",":features:authenticationLock:mpin:mpinauthentication",":common:model", ":common:core",":navigation", ":features:login",":features:home",":common:cache",":features:authenticationLock:fingure",":features:social_login:google_login",":features:settings",":features:search",":features:social_login:fblogin"
+//FeatureModules.settingModules
+include(
+    ":app",
+    ":common:networking",
+    ":libraries:exoplayer",
+    ":libraries:player-core",
+    ":features:profile",
+    ":features:player_feeds",
+    ":features:authenticationLock:mpin:mpinauthentication",
+    ":common:model",
+    ":common:core",
+    ":navigation",
+    ":features:login",
+    ":features:home",
+    ":common:cache",
+    ":features:authenticationLock:fingure",
+    ":features:social_login:google_login",
+    ":features:settings",
+    ":features:search",
+    ":features:social_login:fblogin"
+)
+rootProject.name = "ModuleArchitechure"
